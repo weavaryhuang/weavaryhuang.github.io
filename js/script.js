77f7687bc16b405119a45ad8445a90a1af19d0ed
HM <w1a8a2y@gmail.com>
2023-04-13 16:00:53 +0100

    new

---
+++ js/script.js
@@ -0,0 +1,438 @@
+const nihongana = [];
+const nih_sp = [];
+
+/////////////
+
+nihongana[0] = "あ";
+nihongana[1] = "い";
+nihongana[2] = "う";
+nihongana[3] = "え";
+nihongana[4] = "お";
+
+nihongana[5] = "か";
+nihongana[6] = "き";
+nihongana[7] = "く";
+nihongana[8] = "け";
+nihongana[9] = "こ";
+
+nihongana[10] = "さ";
+nihongana[11] = "し";
+nihongana[12] = "す";
+nihongana[13] = "せ";
+nihongana[14] = "そ";
+
+nihongana[15] = "た";
+nihongana[16] = "ち";
+nihongana[17] = "つ";
+nihongana[18] = "て";
+nihongana[19] = "と";
+
+nihongana[20] = "な";
+nihongana[21] = "に";
+nihongana[22] = "ぬ";
+nihongana[23] = "ね";
+nihongana[24] = "の";
+
+nihongana[25] = "は";
+nihongana[26] = "ひ";
+nihongana[27] = "ふ";
+nihongana[28] = "へ";
+nihongana[29] = "ほ";
+
+nihongana[30] = "ま";
+nihongana[31] = "み";
+nihongana[32] = "む";
+nihongana[33] = "め";
+nihongana[34] = "も";
+
+nihongana[35] = "や";
+nihongana[36] = "ゆ";
+nihongana[37] = "よ";
+
+nihongana[38] = "ら";
+nihongana[39] = "り";
+nihongana[40] = "る";
+nihongana[41] = "れ";
+nihongana[42] = "ろ";
+
+nihongana[43] = "わ";
+nihongana[44] = "を";
+nihongana[45] = "ん";
+
+///////////////
+
+nihongana[46] = "ア";
+nihongana[47] = "イ";
+nihongana[48] = "ウ";
+nihongana[49] = "エ";
+nihongana[50] = "オ";
+
+nihongana[51] = "カ";
+nihongana[52] = "キ";
+nihongana[53] = "ク";
+nihongana[54] = "ケ";
+nihongana[55] = "コ";
+
+nihongana[56] = "サ";
+nihongana[57] = "シ";
+nihongana[58] = "ス";
+nihongana[59] = "セ";
+nihongana[60] = "ソ";
+
+nihongana[61] = "タ";
+nihongana[62] = "チ";
+nihongana[63] = "ツ";
+nihongana[64] = "テ";
+nihongana[65] = "ト";
+
+nihongana[66] = "ナ";
+nihongana[67] = "ニ";
+nihongana[68] = "ヌ";
+nihongana[69] = "ネ";
+nihongana[70] = "ノ";
+
+nihongana[71] = "ハ";
+nihongana[72] = "ヒ";
+nihongana[73] = "フ";
+nihongana[74] = "ヘ";
+nihongana[75] = "ホ";
+
+nihongana[76] = "マ";
+nihongana[77] = "ミ";
+nihongana[78] = "ム";
+nihongana[79] = "メ";
+nihongana[80] = "モ";
+
+nihongana[81] = "ヤ";
+nihongana[82] = "ユ";
+nihongana[83] = "ヨ";
+
+nihongana[84] = "ラ";
+nihongana[85] = "リ";
+nihongana[86] = "ル";
+nihongana[87] = "レ";
+nihongana[88] = "ロ";
+
+nihongana[89] = "ワ";
+nihongana[90] = "ヲ";
+nihongana[91] = "ン";
+
+nihongana[92] = "が";
+nihongana[93] = "ぎ";
+nihongana[94] = "ぐ";
+nihongana[95] = "げ";
+nihongana[96] = "ご";
+
+nihongana[97] = "ざ";
+nihongana[98] = "じ";
+nihongana[99] = "ず";
+nihongana[100] = "ぜ";
+nihongana[101] = "ぞ";
+            
+nihongana[102] = "だ";
+nihongana[103] = "ぢ";
+nihongana[104] = "づ";
+nihongana[105] = "で";
+nihongana[106] = "ど";
+            
+nihongana[107] = "ば";
+nihongana[108] = "び";
+nihongana[109] = "ぶ";
+nihongana[110] = "べ";
+nihongana[111] = "ぼ";
+            
+nihongana[112] = "ガ";
+nihongana[113] = "ギ";
+nihongana[114] = "グ";
+nihongana[115] = "ゲ";
+nihongana[116] = "ゴ";
+            
+nihongana[117] = "ザ";
+nihongana[118] = "ジ";
+nihongana[119] = "ズ";
+nihongana[120] = "ゼ";
+nihongana[121] = "ゾ";
+            
+nihongana[122] = "ダ";
+nihongana[123] = "ヂ";
+nihongana[124] = "ヅ";
+nihongana[125] = "デ";
+nihongana[126] = "ド";
+            
+nihongana[127] = "バ";
+nihongana[128] = "ビ";
+nihongana[129] = "ブ";
+nihongana[130] = "ベ";
+nihongana[131] = "ボ";
+
+///////////////
+
+nih_sp[0] = "a";
+nih_sp[1] = "i";
+nih_sp[2] = "u";
+nih_sp[3] = "e";
+nih_sp[4] = "o";
+
+nih_sp[5] = "ka";
+nih_sp[6] = "ki";
+nih_sp[7] = "ku";
+nih_sp[8] = "ke";
+nih_sp[9] = "ko";
+
+nih_sp[10] = "sa";
+nih_sp[11] = "shi";
+nih_sp[12] = "su";
+nih_sp[13] = "se";
+nih_sp[14] = "so";
+
+nih_sp[15] = "ta";
+nih_sp[16] = "chi";
+nih_sp[17] = "tsu";
+nih_sp[18] = "te";
+nih_sp[19] = "to";
+
+nih_sp[20] = "na";
+nih_sp[21] = "ni";
+nih_sp[22] = "nu";
+nih_sp[23] = "ne";
+nih_sp[24] = "no";
+
+nih_sp[25] = "ha";
+nih_sp[26] = "hi";
+nih_sp[27] = "fu";
+nih_sp[28] = "he";
+nih_sp[29] = "ho";
+
+nih_sp[30] = "ma";
+nih_sp[31] = "mi";
+nih_sp[32] = "mu";
+nih_sp[33] = "me";
+nih_sp[34] = "mo";
+
+nih_sp[35] = "ya";
+nih_sp[36] = "yu";
+nih_sp[37] = "yo";
+
+nih_sp[38] = "ra";
+nih_sp[39] = "ri";
+nih_sp[40] = "ru";
+nih_sp[41] = "re";
+nih_sp[42] = "ro";
+
+nih_sp[43] = "wa";
+nih_sp[44] = "wo";
+nih_sp[45] = "n";
+
+nih_sp[46] = "a";
+nih_sp[47] = "i";
+nih_sp[48] = "u";
+nih_sp[49] = "e";
+nih_sp[50] = "o";
+       
+nih_sp[51] = "ka";
+nih_sp[52] = "ki";
+nih_sp[53] = "ku";
+nih_sp[54] = "ke";
+nih_sp[55] = "ko";
+       
+nih_sp[56] = "sa";
+nih_sp[57] = "shi";
+nih_sp[58] = "su";
+nih_sp[59] = "se";
+nih_sp[60] = "so";
+       
+nih_sp[61] = "ta";
+nih_sp[62] = "chi";
+nih_sp[63] = "tsu";
+nih_sp[64] = "te";
+nih_sp[65] = "to";
+       
+nih_sp[66] = "na";
+nih_sp[67] = "ni";
+nih_sp[68] = "nu";
+nih_sp[69] = "ne";
+nih_sp[70] = "no";
+       
+nih_sp[71] = "ha";
+nih_sp[72] = "hi";
+nih_sp[73] = "fu";
+nih_sp[74] = "he";
+nih_sp[75] = "ho";
+       
+nih_sp[76] = "ma";
+nih_sp[77] = "mi";
+nih_sp[78] = "mu";
+nih_sp[79] = "me";
+nih_sp[80] = "mo";
+       
+nih_sp[81] = "ya";
+nih_sp[82] = "yu";
+nih_sp[83] = "yo";
+       
+nih_sp[84] = "ra";
+nih_sp[85] = "ri";
+nih_sp[86] = "ru";
+nih_sp[87] = "re";
+nih_sp[88] = "ro";
+       
+nih_sp[89] = "wa";
+nih_sp[90] = "wo";
+nih_sp[91] = "n";
+
+nih_sp[92] = "ga";
+nih_sp[93] = "gi";
+nih_sp[94] = "gu";
+nih_sp[95] = "ge";
+nih_sp[96] = "go";
+
+nih_sp[97] = "za";
+nih_sp[98] = "zi";
+nih_sp[99] = "zu";
+nih_sp[100] = "ze";
+nih_sp[101] = "zo";
+         
+nih_sp[102] = "da";
+nih_sp[103] = "zi";
+nih_sp[104] = "zu";
+nih_sp[105] = "de";
+nih_sp[106] = "do";
+         
+nih_sp[107] = "ba";
+nih_sp[108] = "bi";
+nih_sp[109] = "bu";
+nih_sp[110] = "be";
+nih_sp[111] = "bo";
+         
+nih_sp[112] = "ga";
+nih_sp[113] = "gi";
+nih_sp[114] = "gu";
+nih_sp[115] = "ge";
+nih_sp[116] = "go";
+              
+nih_sp[117] = "za";
+nih_sp[118] = "zi";
+nih_sp[119] = "zu";
+nih_sp[120] = "ze";
+nih_sp[121] = "zo";
+         
+nih_sp[122] = "da";
+nih_sp[123] = "zi";
+nih_sp[124] = "zu";
+nih_sp[125] = "de";
+nih_sp[126] = "do";
+              
+nih_sp[127] = "ba";
+nih_sp[128] = "bi";
+nih_sp[129] = "bu";
+nih_sp[130] = "be";
+nih_sp[131] = "bo";
+
+///////////////////
+
+var checkFlag = 0 ;
+var checkFlag = 0 ;
+var score = 0;
+var count = 0;
+var num = 46;
+var num_offset = 0;
+var mode_string = "Hiragana mode";
+
+var flag_hir = 1;  
+var flag_kat = 0;  
+var flag_oth = 0;  
+var flag_ath = 0;  
+
+var randomNum = randomMake();
+document.getElementById('demo').innerHTML = nihongana[randomNum];
+document.getElementById('mode').innerHTML = "Hiragana mode";
+document.getElementById('mode').style.color = 'coral';
+
+function hiraganamode(){
+    num = 46;
+    num_offset = 0;
+    mode_string = "Hiragana mode";
+    document.getElementById('mode').innerHTML = "Hiragana mode";
+    document.getElementById('mode').style.color = 'coral';
+    flag_hir = !flag_hir;
+}
+
+function katakanamode(){
+    num = 46;
+    num_offset = 46;
+    mode_string = "Hiragana mode";    
+    document.getElementById('mode').innerHTML = "Katakana mode";
+    document.getElementById('mode').style.color = 'blue';
+    flag_kat = !flag_kat;
+}
+
+function othermode(){
+    num = 40;
+    num_offset = 92;
+    mode_string = "Hiragana mode";
+    document.getElementById('mode').innerHTML = "Other mode";
+    document.getElementById('mode').style.color = 'red';
+    flag_oth = !flag_oth;
+}
+
+function allmode(){
+    num = 132;
+    num_offset = 0;
+    mode_string = "Hiragana mode";
+    document.getElementById('mode').innerHTML = "All mode";
+    document.getElementById('mode').style.color = 'black';
+    flag_ath = !flag_ath;
+}
+
+function randomMake(){
+    return (Math.floor(Math.random()*num) + num_offset);
+}
+
+function checkValue(){
+    //var input = document.getElementById("answer");
+    input.addEventListener("keypress", function(event) {
+    if (event.key === " ") {
+     event.preventDefault();
+     if (input.value == nih_sp[randomNum]){
+      checkFlag = 1;  
+      document.getElementById('correction').innerHTML = 'Correct answer!!';
+      document.getElementById('correction').style.color = 'green';
+      //alert(checkFlag);
+      randomNum = randomMake();
+      document.getElementById('demo').innerHTML = nihongana[randomNum];
+      input.value = "";
+      score += 1;
+      count += 1;
+      document.getElementById('score').innerHTML = score;
+      document.getElementById('total').innerHTML = count;
+      document.getElementById('rate').innerHTML = Math.floor((score/count)*100);
+      document.getElementById('bar').value = Math.floor((score/count)*100);
+     }
+     else{
+      checkFlag = 0;
+      document.getElementById('correction').innerHTML = 'Wrong word! correct is ' + nih_sp[randomNum];
+      document.getElementById('correction').style.color = 'crimson';
+      //alert(checkFlag);
+      randomNum = randomMake();
+      document.getElementById('demo').innerHTML = nihongana[randomNum];
+      input.value = "";
+      count += 1;
+      document.getElementById('total').innerHTML = randomNum;
+      document.getElementById('rate').innerHTML = Math.floor((score/count)*100);
+      document.getElementById('bar').value = Math.floor((score/count)*100);
+     }
+     }
+    });
+}
+
+function clearAll(){
+    score = 0;
+    total = 0;
+    document.getElementById('score').innerHTML = 0;
+    document.getElementById('total').innerHTML = 0;
+    document.getElementById('rate').innerHTML = 0; 
+    document.getElementById('bar').value = 0;
+}
+
+function timeUp() {
+    document.getElementById("demo").innerHTML = "Score: " + score;
+}
